ac3d0d4dfa505b5908e014898fdc8a7bdb39eaee
Gillette Aziabah <vgil.aziabah@gmail.com>
2022-10-13 18:25:06 +0000

    Final work for now!!!

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-let firstCard = 10
-let secondCard = 12
-let sum = firstCard + secondCard 
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEL = document.querySelector("#message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector(".cards-el")
 
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    }else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Card:  " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+        
+    }
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
@@ -21,7 +48,16 @@ function startGame() {
 
 messageEL.textContent = message
 sumEl.textContent = "Sum: " + sum
-cardsEl.textContent = "Card:  " + firstCard + " " +secondCard
+
 }
 
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+         renderGame()  
+    }
+
+}
 
